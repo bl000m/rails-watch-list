576e63e5b38dd16514a1855a54e56b8ba189d10a
mathia pagani <mathiapagani@gmail.com>
2022-05-20 12:39:35 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# get 'lists', to: 'lists#index'
+# get 'lists/:id', to: 'lists#show', as: :list
+# get 'lists/new', to: 'lists#new', as: :new_list
+# post 'restaurants', to: 'lists#create'
+  resources :lists, only: %i[create index show new] do
+    resources :bookmarks, only: %i[create new]
+  end
+  resources :bookmarks, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+require "json"
+require "open-uri"
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +9,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+url = "http://tmdb.lewagon.com/movie/top_rated?"
+movies_db = URI.open(url).read
+movies = JSON.parse(movies_db)
+results = movies['results']
+
+results[0..10].each do |result|
+  Movie.create!(title: result['title'], overview: result['overview'], poster_url: result['poster_path'], rating: result['rating'])
+end
+
+10.times do
+  List.create(name: Faker::Creature::Animal.name)
+end
